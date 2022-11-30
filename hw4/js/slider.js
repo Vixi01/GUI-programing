8f9715b59dc55c3393310dfb7632afbf27219b06
Brian Cheng <113400402+Vixi01@users.noreply.github.com>
2022-11-30 01:08:06 -0500

    Update slider.js

--- hw4/js/slider.js
+++ hw4/js/slider.js
@@ -1,54 +1,64 @@
+// File: slider.js
+// GUI Assignment: Dynamic Multiplication Table
+// Brian Cheng, UMass Lowell Computer Science, Haojen_cheng@student.uml.edu
+// What to submit: all program code
+// Copyright (c) 2022 by Brian. All rights reserved. May be freely copied or
+// excerpted for educational purposes with credit to the author.
+// updated by Brian on Nov 30, 2022 at 12:12 AM
 $().ready(function () {
 
-    $("#slider_fnr").slider({
-      min: -500,
-      max: 500,
-      slide: function (event, ui) {
-        $("#fnr").val(ui.value);
-      }
-    });
-    bind("#slider_fnr","#fnr");
-    
-
-    $("#slider_lnr").slider({
-      min: -500,
-      max: 500,
-      slide: function (event, ui) {
-        $("#lnr").val(ui.value);
-      }
-    });
-    bind("#slider_lnr","#lnr");
-
-    $("#slider_fnc").slider({
-      min: -500,
-      max: 500,
-      slide: function (event, ui) {
-        $("#fnc").val(ui.value);
-      }
-    });
-    bind("#slider_fnc","#fnc");
+  $("#slider_fnr").slider({          // create the fnr slider
+    min: -100,
+    max: 100,
+    slide: function (event, ui) {    // Binding textbox to slider
+      $("#fnr").val(ui.value);
+      AutoSubmit()
+    }
+  });
+  bind("#slider_fnr", "#fnr");
 
 
+  $("#slider_lnr").slider({           // create the lnr slider
+    min: -100,
+    max: 100,
+    slide: function (event, ui) {     // Binding textbox to slider
+      $("#lnr").val(ui.value);
+      AutoSubmit()
+    }
+  });
+  bind("#slider_lnr", "#lnr");
 
-    $("#slider_lnc").slider({
-      min: -500,
-      max: 500,
-      slide: function (event, ui) {
-        $("#lnc").val(ui.value);
-      }
-    });
-    bind("#slider_lnc","#lnc");
+  $("#slider_fnc").slider({           // create the fnc slider
+    min: -100,
+    max: 100,
+    slide: function (event, ui) {     // Binding textbox to slider
+      $("#fnc").val(ui.value);
+      AutoSubmit()
+    }
+  });
+  bind("#slider_fnc", "#fnc");
 
-    
+
+
+  $("#slider_lnc").slider({           // create the lnc slider
+    min: -100,
+    max: 100,
+    slide: function (event, ui) {     // Binding textbox to slider
+      $("#lnc").val(ui.value);
+      AutoSubmit()
+    }
+  });
+  bind("#slider_lnc", "#lnc");
+
+
 });
-function bind(x,b) {
+function bind(x, b) {                    // Binding slider to textbox
 
-    var initialValue = $(x).slider("option", "value");
-    $(b).val(initialValue);
-    $(b).change(function () {
-        var oldVal = $(x).slider("option", "value");
-        var newVal = $(this).val();
-        $(x).slider("option", "value", newVal);
-    });
-  
+  var initialValue = $(x).slider("option", "value");
+  $(b).val(initialValue);
+  $(b).change(function () {
+    var newVal = $(this).val();
+    $(x).slider("option", "value", newVal);
+    AutoSubmit()
+  });
 };
